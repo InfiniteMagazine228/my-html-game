698608fe05da1369ad6779c23c3cf7923609678f
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-05-05 19:12:52 +0700

    Initialize Express server with Socket.IO support

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+let players = {};
+
+io.on("connection", (socket) => {
+  console.log("Người chơi kết nối:", socket.id);
+  players[socket.id] = { x:0, y:0, z:0 };
+
+  socket.on("move", (pos) => {
+    players[socket.id] = pos;
+    io.emit("update", players);
+  });
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    io.emit("update", players);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server chạy ở cổng 3000");
+});
